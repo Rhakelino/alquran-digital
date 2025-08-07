d70da67efb4f525338c92e43d0a3d90f8835afe6
rakel lino <instanclay@gmail.com>
2025-08-07 07:04:13 +0700

    change favicon

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v6sk5oh9to8"
+    "revision": "0.gqv3v8oec0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
